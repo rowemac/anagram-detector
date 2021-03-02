4b4e7057d4a00e34b74f3c1ab4959707af50e5b1
Mac Rowe <m.rowe42@gmail.com>
2021-03-02 12:47:57 -0500

    commit tree 464af27b47c007c5301a5367cc21a245c4f2d651

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
 # Your code goes here!
+require 'pry'
+
+
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(word_array)
+       
+        new_array = []
+        
+        word_array.each do | each_word |
+            if each_word.split('').sort == @word.split('').sort
+                new_array << each_word
+            end
+        end
+        new_array
+    end
+
+end
+
